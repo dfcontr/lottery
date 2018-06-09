1e04f957fb48d9d9654df3eeb8a3ba4cf3e83e8c
Daniel Contreras <daniel.contreras@outlook.com>
2018-06-09 10:59:47 -0600

    Initial commit

---
+++ deploy.js
@@ -0,0 +1,27 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'decline poverty swift chalk popular warm hundred hair protect hundred arrive ability',
+  'https://rinkeby.infura.io/2yCBWKlXvm4lchwqvmDQ'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log('Attempting to deploy from account: ', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: '0x' + bytecode })
+    .send({
+      gas: '1000000',
+      gasPrice: web3.utils.toWei('2', 'gwei'),
+      from: accounts[0]
+    });
+
+  console.log(interface);
+  console.log('Contract deployed to: ', result.options.address);
+};
+deploy();
